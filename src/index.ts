b6dbd41d5e81659bb6816f559af7892e2cfdd219
Matthew Murphy <matt@mattmurphy.ca>
2021-04-14 18:56:38 -0600

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,8 @@
-import { Project } from './Project'
+//
+// * IMPORTS
+
+//
+// * PROGRAM
 
 //
 // * FUNCTIONS
-
-function getProjectNameFromParameters(): string {
-	// $ create-node-project [name of project with spaces]
-	const projectName = process.argv.slice(2).join(' ') // name-of-project-with-spaces
-
-	if (projectName) {
-		return projectName
-	} else
-		throw new Error(
-			'Please provide a project name:\n\tcreate-node-project [directory] [project-name]',
-		)
-}
-
-//
-// * PROGRAM START
-
-const projectName = getProjectNameFromParameters()
-new Project(projectName)
